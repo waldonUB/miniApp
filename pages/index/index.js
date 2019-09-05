c82d59041f3764e8bf350878b5d034c6506ce7bf
waldonUB <852727515@qq.com>
2019-09-05 16:40:57 +0800

    提交缓存测试

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
   //事件处理函数
   bindViewTap: function() {
